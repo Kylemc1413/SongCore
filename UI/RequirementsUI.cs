57a8625ef2effe5e4f3c8085ef0434de0a1b5261
Auros Nexus <aurosnex@gmail.com>
2019-12-13 17:58:32 -0500

    BSML Integration
    yey

---
+++ UI/RequirementsUI.cs
@@ -0,0 +1,40 @@
+﻿using BeatSaberMarkupLanguage;
+using BS_Utils.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SongCore.UI
+{
+    public class RequirementsUI : PersistentSingleton<RequirementsUI>
+    {
+        private StandardLevelDetailViewController standardLevel;
+
+
+        internal static Config ModPrefs = new Config("SongCore/SongCore");
+        internal Button infoButton;
+
+        internal Sprite HaveReqIcon;
+        internal Sprite MissingReqIcon;
+        internal Sprite HaveSuggestionIcon;
+        internal Sprite MissingSuggestionIcon;
+        internal Sprite WarningIcon;
+        internal Sprite InfoIcon;
+        internal Sprite MissingCharIcon;
+        internal Sprite LightshowIcon;
+        internal Sprite ExtraDiffsIcon;
+        internal Sprite WIPIcon;
+        internal Sprite FolderIcon;
+
+        internal void Setup()
+        {
+            standardLevel = Resources.FindObjectsOfTypeAll<StandardLevelDetailViewController>().First();
+            BSMLParser.instance.Parse(BeatSaberMarkupLanguage.Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), "SongCore.UI.requirements.bsml"), standardLevel.gameObject, this);
+        }
+    }
+}
