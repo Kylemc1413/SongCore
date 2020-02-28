a67e28e952e2ac5297867b5de60d4da2d38aed48
Kyle1413 <kylet1412@gmail.com>
2020-02-28 13:06:59 -0500

    Correct Pack Cover ppu

--- OverrideClasses/SongCoreCustomBeatmapLevelPack.cs
+++ OverrideClasses/SongCoreCustomBeatmapLevelPack.cs
@@ -1,9 +1,13 @@
-﻿namespace SongCore.OverrideClasses
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace SongCore.OverrideClasses
 {
     public class SongCoreCustomBeatmapLevelPack : CustomBeatmapLevelPack
     {
         public SongCoreCustomBeatmapLevelPack(string packID, string packName, UnityEngine.Sprite coverImage, CustomBeatmapLevelCollection customBeatmapLevelCollection, string shortPackName = "") : base(packID, packName, shortPackName, coverImage, customBeatmapLevelCollection)
         {
+            coverImage = Sprite.Create(coverImage.texture, coverImage.rect, coverImage.pivot, coverImage.texture.width);
+            _coverImage = coverImage;
             if (shortPackName == "")
                 _shortPackName = packName;
         }
