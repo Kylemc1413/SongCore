0aad81fb97419bef62ebdc75d4aec5123986be0c
Auros Nexus <aurosnex@gmail.com>
2021-02-04 22:36:01 -0500

    Zenjecty Loaded Event

--- HarmonyPatches/MainSystemsInitRefreshablePatch.cs
+++ HarmonyPatches/MainSystemsInitRefreshablePatch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System;
+using System.Collections.Concurrent;
 using Zenject;
 
 namespace SongCore.HarmonyPatches
@@ -6,9 +8,15 @@ namespace SongCore.HarmonyPatches
     [HarmonyPatch(typeof(MainSystemInit), nameof(MainSystemInit.InstallBindings))]
     public class MainSystemsInitRefreshablePatch
     {
+        public const string refreshableID = "SongCore.Loader.Refresh";
+        public const string didLoadEventID = "SongCore.Loader.Loaded";
+
         static void Postfix(DiContainer container)
         {
-            container.Bind<IRefreshable>().WithId($"{nameof(SongCore)}.{nameof(Loader)}").To<SongCoreRefreshable>().AsSingle();
+            container.Bind<IRefreshable>().WithId(refreshableID).To<SongCoreRefreshable>().AsSingle();
+            container.Bind(typeof(IInitializable), typeof(IDisposable), typeof(SongCoreLoaderDidLoad)).To<SongCoreLoaderDidLoad>().AsSingle();
+            IObservableChange loadEvent = container.Resolve<SongCoreLoaderDidLoad>();
+            container.BindInstance(loadEvent).WithId(didLoadEventID).AsSingle();
         }
 
         private class SongCoreRefreshable : IRefreshable
@@ -19,6 +27,26 @@ namespace SongCore.HarmonyPatches
                 {
                     Loader.Instance.RefreshSongs();
                 }
+            }
+        }
+
+        private class SongCoreLoaderDidLoad : IInitializable, IDisposable, IObservableChange
+        {
+            public event Action didChangeEvent;
+
+            public void Initialize()
+            {
+                Loader.SongsLoadedEvent += Loader_SongsLoadedEvent;
+            }
+
+            private void Loader_SongsLoadedEvent(Loader _, ConcurrentDictionary<string, CustomPreviewBeatmapLevel> __)
+            {
+                didChangeEvent?.Invoke();
+            }
+
+            public void Dispose()
+            {
+                Loader.SongsLoadedEvent -= Loader_SongsLoadedEvent;
             }
         }
     }
