fe648f299242743b7eaff82db77b2cb58fdf8ea9
Auros Nexus <aurosnex@gmail.com>
2021-02-04 22:01:46 -0500

    Created harmony patch

---
+++ HarmonyPatches/MainSystemsInitRefreshablePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Zenject;
+
+namespace SongCore.HarmonyPatches
+{
+    [HarmonyPatch(typeof(MainSystemInit), nameof(MainSystemInit.InstallBindings))]
+    public class MainSystemsInitRefreshablePatch
+    {
+        static void Postfix(DiContainer container)
+        {
+            container.Bind<IRefreshable>().WithId($"{nameof(SongCore)}.{nameof(Loader)}").To<SongCoreRefreshable>().AsSingle();
+        }
+
+        private class SongCoreRefreshable : IRefreshable
+        {
+            public void Refresh()
+            {
+                if (Loader.AreSongsLoaded)
+                {
+                    Loader.Instance.RefreshSongs();
+                }
+            }
+        }
+    }
+}
