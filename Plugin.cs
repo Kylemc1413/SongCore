33af88ad49728e7232d09ea4f5c711a3b35bd4e6
Kylemc1413 <kylet1412@gmail.com>
2021-09-23 17:13:39 -0400

    Uncomment Requirements UI

--- Plugin.cs
+++ Plugin.cs
@@ -91,7 +91,7 @@ namespace SongCore
         private void BSEvents_menuSceneLoadedFresh(ScenesTransitionSetupDataSO data)
         {
             Loader.OnLoad();
-           // RequirementsUI.instance.Setup();
+            RequirementsUI.instance.Setup();
         }
 
         private void BSEvents_gameSceneLoaded()
