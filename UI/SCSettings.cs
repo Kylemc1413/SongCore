080ebdd9d0d5d1ed311dc140a0b6e5044e936ef7
PixelBoom <rithikbhatia12@gmail.com>
2021-12-06 02:36:11 -0500

    Revert "Moved custom colors toggle to the colors tab"
    This reverts commit fd537bb4170f2c15340a474558a6b75b65a0e968.
    
    # Conflicts:
    #	UI/ColorsUI.cs
    #	UI/colors.bsml

--- UI/SCSettings.cs
+++ UI/SCSettings.cs
@@ -1,9 +1,20 @@
-using BeatSaberMarkupLanguage.Attributes;
+﻿using BeatSaberMarkupLanguage.Attributes;
 
 namespace SongCore.UI
 {
     public class SCSettings : PersistentSingleton<SCSettings>
     {
+        [UIValue("colors")]
+        public bool Colors
+        {
+            get => BasicUI.ModPrefs.GetBool("SongCore", "customSongColors", true, true);
+            set
+            {
+                Plugin.CustomSongColors = value;
+                BasicUI.ModPrefs.SetBool("SongCore", "customSongColors", value);
+            }
+        }
+
         [UIValue("platforms")]
         public bool Platforms
         {
