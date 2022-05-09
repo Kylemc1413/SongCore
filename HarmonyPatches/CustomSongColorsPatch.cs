d964a40f919387042fea67655d5735d9f34d25f3
Jonathon Fitch <lightman1@virginmedia.com>
2022-05-09 18:35:59 +0100

    Optimize CustomSongColors prefix to avoid unnecessary operations

--- HarmonyPatches/CustomSongColorsPatch.cs
+++ HarmonyPatches/CustomSongColorsPatch.cs
@@ -9,8 +9,10 @@ namespace SongCore.HarmonyPatches
     {
         private static void Prefix(ref IDifficultyBeatmap difficultyBeatmap, ref ColorScheme? overrideColorScheme)
         {
-            var environmentInfoSO = difficultyBeatmap.GetEnvironmentInfo();
-            var fallbackScheme = overrideColorScheme ?? new ColorScheme(environmentInfoSO.colorScheme);
+            if (!Plugin.Configuration.CustomSongColors)
+            {
+                return;
+            }
 
             var songData = Collections.RetrieveDifficultyData(difficultyBeatmap);
             if (songData == null)
@@ -23,23 +25,23 @@ namespace SongCore.HarmonyPatches
                 return;
             }
 
-            if (Plugin.Configuration.CustomSongColors)
-            {
-                Logging.Logger.Info("Custom Song Colors On");
-                var saberLeft = songData._colorLeft == null ? fallbackScheme.saberAColor : Utils.ColorFromMapColor(songData._colorLeft);
-                var saberRight = songData._colorRight == null ? fallbackScheme.saberBColor : Utils.ColorFromMapColor(songData._colorRight);
-                var envLeft = songData._envColorLeft == null
-                    ? songData._colorLeft == null ? fallbackScheme.environmentColor0 : Utils.ColorFromMapColor(songData._colorLeft)
-                    : Utils.ColorFromMapColor(songData._envColorLeft);
-                var envRight = songData._envColorRight == null
-                    ? songData._colorRight == null ? fallbackScheme.environmentColor1 : Utils.ColorFromMapColor(songData._colorRight)
-                    : Utils.ColorFromMapColor(songData._envColorRight);
-                var envLeftBoost = songData._envColorLeftBoost == null ? envLeft : Utils.ColorFromMapColor(songData._envColorLeftBoost);
-                var envRightBoost = songData._envColorRightBoost == null ? envRight : Utils.ColorFromMapColor(songData._envColorRightBoost);
-                var obstacle = songData._obstacleColor == null ? fallbackScheme.obstaclesColor : Utils.ColorFromMapColor(songData._obstacleColor);
-                overrideColorScheme = new ColorScheme("SongCoreMapColorScheme", "SongCore Map Color Scheme", true, "SongCore Map Color Scheme", false, saberLeft, saberRight, envLeft,
-                    envRight, true, envLeftBoost, envRightBoost, obstacle);
-            }
+            var environmentInfoSO = difficultyBeatmap.GetEnvironmentInfo();
+            var fallbackScheme = overrideColorScheme ?? new ColorScheme(environmentInfoSO.colorScheme);
+
+            Logging.Logger.Info("Custom Song Colors On");
+            var saberLeft = songData._colorLeft == null ? fallbackScheme.saberAColor : Utils.ColorFromMapColor(songData._colorLeft);
+            var saberRight = songData._colorRight == null ? fallbackScheme.saberBColor : Utils.ColorFromMapColor(songData._colorRight);
+            var envLeft = songData._envColorLeft == null
+                ? songData._colorLeft == null ? fallbackScheme.environmentColor0 : Utils.ColorFromMapColor(songData._colorLeft)
+                : Utils.ColorFromMapColor(songData._envColorLeft);
+            var envRight = songData._envColorRight == null
+                ? songData._colorRight == null ? fallbackScheme.environmentColor1 : Utils.ColorFromMapColor(songData._colorRight)
+                : Utils.ColorFromMapColor(songData._envColorRight);
+            var envLeftBoost = songData._envColorLeftBoost == null ? envLeft : Utils.ColorFromMapColor(songData._envColorLeftBoost);
+            var envRightBoost = songData._envColorRightBoost == null ? envRight : Utils.ColorFromMapColor(songData._envColorRightBoost);
+            var obstacle = songData._obstacleColor == null ? fallbackScheme.obstaclesColor : Utils.ColorFromMapColor(songData._obstacleColor);
+            overrideColorScheme = new ColorScheme("SongCoreMapColorScheme", "SongCore Map Color Scheme", true, "SongCore Map Color Scheme", false, saberLeft, saberRight, envLeft,
+                envRight, true, envLeftBoost, envRightBoost, obstacle);
         }
     }
 }
