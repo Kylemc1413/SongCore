816d7f614d108918fef91e92498f7297411f4285
Kinsi <kinsi55@users.noreply.github.com>
2022-12-14 00:57:22 +0100

    Show levelAuthorName for all Levels

--- HarmonyPatches/LevelSelectionPatch.cs
+++ HarmonyPatches/LevelSelectionPatch.cs
@@ -7,23 +7,18 @@ namespace SongCore.HarmonyPatches
     [HarmonyPatch("SetDataFromLevelAsync", MethodType.Normal)]
     internal class LevelListTableCellSetDataFromLevel
     {
-        private static void Postfix(IPreviewBeatmapLevel level, bool isFavorite, ref TextMeshProUGUI ____songAuthorText, ref TextMeshProUGUI ____songBpmText)
+        private static void Postfix(IPreviewBeatmapLevel level, TextMeshProUGUI ____songAuthorText, TextMeshProUGUI ____songBpmText)
         {
             // Rounding BPM display for all maps, including official ones
             ____songBpmText.text = System.Math.Round(level.beatsPerMinute).ToString();
 
-            if (!(level is CustomPreviewBeatmapLevel customLevel))
-            {
-                return;
-            }
-
             /* Plan B
            IPA.Utilities.Async.UnityMainThreadTaskScheduler.Factory.StartNew( async () => { var token = new CancellationTokenSource().Token; var audio = await level.GetPreviewAudioClipAsync(token); customLevel.SetField("_songDuration", audio.length);  ____songDurationText.text = customLevel.songDuration.MinSecDurationText(); });
             */
-            ____songAuthorText.richText = true;
-            if (!string.IsNullOrWhiteSpace(customLevel.levelAuthorName))
+            if (!string.IsNullOrWhiteSpace(level.levelAuthorName))
             {
-                ____songAuthorText.text = $"<size=80%>{customLevel.songAuthorName}</size> <size=90%>[<color=#89ff89>{customLevel.levelAuthorName.Replace(@"<", "<\u200B").Replace(@">", ">\u200B")}</color>]</size>";
+                ____songAuthorText.richText = true;
+                ____songAuthorText.text = $"<size=80%>{level.songAuthorName}</size> <size=90%>[<color=#89ff89>{level.levelAuthorName.Replace(@"<", "<\u200B").Replace(@">", ">\u200B")}</color>]</size>";
             }
         }
     }
