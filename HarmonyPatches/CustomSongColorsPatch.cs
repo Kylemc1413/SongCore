f5d3cea598c195a194eea1749cc5c7d3e9e6576c
kaitlyn~ <87990853+ItsKaitlyn03@users.noreply.github.com>
2023-02-22 18:48:15 -0500

    Allow for assigning environment ColorW + boost (#95)
    * Allow for assigning environment ColorW + boost
    
    * Oops

--- HarmonyPatches/CustomSongColorsPatch.cs
+++ HarmonyPatches/CustomSongColorsPatch.cs
@@ -1,5 +1,7 @@
 using HarmonyLib;
+using IPA.Utilities;
 using SongCore.Utilities;
+using Utils = SongCore.Utilities.Utils;
 
 namespace SongCore.HarmonyPatches
 {
@@ -20,7 +22,7 @@ namespace SongCore.HarmonyPatches
                 return;
             }
 
-            if (songData._colorLeft == null && songData._colorRight == null && songData._envColorLeft == null && songData._envColorRight == null && songData._obstacleColor == null && songData._envColorLeftBoost == null && songData._envColorRightBoost == null)
+            if (songData._colorLeft == null && songData._colorRight == null && songData._envColorLeft == null && songData._envColorRight == null && songData._envColorWhite == null && songData._obstacleColor == null && songData._envColorLeftBoost == null && songData._envColorRightBoost == null && songData._envColorWhiteBoost == null)
             {
                 return;
             }
@@ -37,11 +39,15 @@ namespace SongCore.HarmonyPatches
             var envRight = songData._envColorRight == null
                 ? songData._colorRight == null ? fallbackScheme.environmentColor1 : Utils.ColorFromMapColor(songData._colorRight)
                 : Utils.ColorFromMapColor(songData._envColorRight);
+            var envWhite = songData._envColorWhite == null ? fallbackScheme.environmentColorW : Utils.ColorFromMapColor(songData._envColorWhite);
             var envLeftBoost = songData._envColorLeftBoost == null ? envLeft : Utils.ColorFromMapColor(songData._envColorLeftBoost);
             var envRightBoost = songData._envColorRightBoost == null ? envRight : Utils.ColorFromMapColor(songData._envColorRightBoost);
+            var envWhiteBoost = songData._envColorWhiteBoost == null ? fallbackScheme.environmentColorWBoost : Utils.ColorFromMapColor(songData._envColorWhiteBoost);
             var obstacle = songData._obstacleColor == null ? fallbackScheme.obstaclesColor : Utils.ColorFromMapColor(songData._obstacleColor);
             overrideColorScheme = new ColorScheme("SongCoreMapColorScheme", "SongCore Map Color Scheme", true, "SongCore Map Color Scheme", false, saberLeft, saberRight, envLeft,
                 envRight, true, envLeftBoost, envRightBoost, obstacle);
+            overrideColorScheme.SetField("_environmentColorW", envWhite);
+            overrideColorScheme.SetField("_environmentColorWBoost", envWhiteBoost);
         }
     }
 }
