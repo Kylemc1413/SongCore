3bf3fba54e8994b8c628d7e504d88f07dabf7853
Eris <erisapps@outlook.be>
2023-05-03 23:33:38 +0200

    Setup Nuke build
    - Leverage GitVersion
    - Add GH actions workflow files

---
+++ build/Build.CI.GitHubActions.cs
@@ -0,0 +1,30 @@
+﻿using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+
+[GitHubActions(
+	"pr",
+	GitHubActionsImage.UbuntuLatest,
+	AutoGenerate = false,
+	CacheKeyFiles = new string[0],
+	EnableGitHubToken = false,
+	FetchDepth = 0, // Only a single commit is fetched by default, for the ref/SHA that triggered the workflow. Make sure to fetch whole git history, in order to get GitVersion to work.
+	ImportSecrets = new[] { "SIRA_SERVER_CODE" },
+	InvokedTargets = new[] { nameof(Compile) },
+	OnPushBranches = new[] { "main" },
+	OnPullRequestBranches = new[] { "main" },
+	PublishArtifacts = true)]
+[GitHubActions(
+	"publish",
+	GitHubActionsImage.UbuntuLatest,
+	AutoGenerate = false,
+	CacheKeyFiles = new string[0],
+	EnableGitHubToken = true,
+	FetchDepth = 0, // Only a single commit is fetched by default, for the ref/SHA that triggered the workflow. Make sure to fetch whole git history, in order to get GitVersion to work.
+	ImportSecrets = new[] { "SIRA_SERVER_CODE" },
+	InvokedTargets = new[] { nameof(CreateGitHubRelease) },
+	OnPushTags = new[] { "*.*.*" },
+	PublishArtifacts = true)]
+partial class Build
+{
+	[CI] readonly GitHubActions GitHubActions;
+}
