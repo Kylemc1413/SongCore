667071e656302b83fc40892ad95d2b761c97057a
Meivyn <793322+Meivyn@users.noreply.github.com>
2023-07-01 17:21:20 -0400

    Revert `BeatmapVersionDetectionPatch` changes, parsing with Regex is faster

--- source/SongCore/HarmonyPatches/BeatmapVersionDetectionPatch.cs
+++ source/SongCore/HarmonyPatches/BeatmapVersionDetectionPatch.cs
@@ -1,24 +1,35 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using HarmonyLib;
 using JetBrains.Annotations;
-using UnityEngine;
 
 namespace SongCore.HarmonyPatches
 {
     // This patch fixes the base game implementation, which fails with maps that have no version declared.
-    // Without this patch affected maps don't load when selected.
+    // Without this patch affected maps don't load when selected. It is also 100 times faster on average.
     [HarmonyPatch(typeof(BeatmapSaveDataHelpers), nameof(BeatmapSaveDataHelpers.GetVersion))]
     [UsedImplicitly]
     internal static class BeatmapVersionDetectionPatch
     {
+        private static readonly Regex VersionRegex = new Regex(
+            @"""_?version""\s*:\s*""(?<version>[0-9]+\.[0-9]+\.?[0-9]?)""",
+            RegexOptions.Compiled | RegexOptions.CultureInvariant
+        );
+
         private static readonly Version FallbackVersion = new Version("2.0.0");
 
         [UsedImplicitly]
         private static bool Prefix(string data, ref Version __result)
         {
-            string? version = JsonUtility.FromJson<BeatmapSaveDataHelpers.VersionSerializedData>(data).v;
-            __result = version != null ? new Version(version) : FallbackVersion;
+            var truncatedText = data.Substring(0, 50);
+            var match = VersionRegex.Match(truncatedText);
+            if (!match.Success)
+            {
+                __result = FallbackVersion;
+                return false;
+            }
 
+            __result = new Version(match.Groups["version"].Value);
             return false;
         }
     }
