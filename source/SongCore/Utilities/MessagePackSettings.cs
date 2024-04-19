3acaa0929a48282782ac0883f2c5d33279ac42b3
Meivyn <793322+Meivyn@users.noreply.github.com>
2024-04-19 15:53:08 -0400

    Forgot to commit this file

---
+++ source/SongCore/Utilities/MessagePackSettings.cs
@@ -0,0 +1,9 @@
+﻿using MessagePack;
+
+namespace SongCore.Utilities
+{
+    internal static class MessagePackSettings
+    {
+        public static readonly MessagePackSerializerOptions StandardWithCompression = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
+    }
+}
