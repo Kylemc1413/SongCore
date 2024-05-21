8b3203f3ea0764fa60513713f02a061f83761ffb
Meivyn <793322+Meivyn@users.noreply.github.com>
2024-05-21 16:11:40 -0400

    Fix loading of covers & bump version

---
+++ source/SongCore/HarmonyPatches/CoversLoadingFixPatch.cs
@@ -0,0 +1,20 @@
+﻿using BGLib.DotnetExtension.Collections;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SongCore.HarmonyPatches
+{
+    /// <summary>
+    /// This partially fixes a bug that was introduced in v1.36.0, which saves null covers when the loading operation is canceled.
+    /// It still shows the default cover at times, but at least it reloads it now.
+    /// </summary>
+    // TODO: Remove when fixed.
+    [HarmonyPatch(typeof(LRUCache<string, Sprite>), nameof(LRUCache<string, Sprite>.Add))]
+    internal class CoversLoadingFixPatch
+    {
+        private static bool Prefix(Sprite value)
+        {
+            return value != null;
+        }
+    }
+}
