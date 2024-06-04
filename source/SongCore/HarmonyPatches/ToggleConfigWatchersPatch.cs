e7b6f57a01a43307cf2efcbbd6ac06b64ff1c9d4
Meivyn <793322+Meivyn@users.noreply.github.com>
2024-06-04 12:34:38 -0400

    Disable BSIPA config watchers when GC is disabled

---
+++ source/SongCore/HarmonyPatches/ToggleConfigWatchersPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using IPA.Config;
+
+namespace SongCore.HarmonyPatches
+{
+    /// <summary>
+    /// This patch disables BSIPA's monitoring of config files in the game scene.
+    /// It leaks a lot of memory when there's a ton of files to watch.
+    /// </summary>
+    [HarmonyPatch(typeof(DisableGCWhileEnabled))]
+    internal class ToggleConfigWatchersPatch
+    {
+        [HarmonyPatch(nameof(DisableGCWhileEnabled.OnEnable))]
+        private static void Prefix()
+        {
+            ConfigWatchersHelper.ToggleWatchers();
+        }
+
+        [HarmonyPatch(nameof(DisableGCWhileEnabled.OnDisable))]
+        private static void Postfix()
+        {
+            ConfigWatchersHelper.ToggleWatchers();
+        }
+    }
+}
