f811fb507caa4e4ad04250018bd7ce2265ed6acb
Meivyn <793322+Meivyn@users.noreply.github.com>
2024-07-15 14:35:00 -0400

    Disable submission when using `--auto_play` launch argument

---
+++ source/SongCore/HarmonyPatches/AutoplayDisableSubmissionPatch.cs
@@ -0,0 +1,27 @@
+﻿using BeatSaber.Init;
+using BGLib.DotnetExtension.CommandLine;
+using SiraUtil.Submissions;
+using Zenject;
+
+namespace SongCore.HarmonyPatches
+{
+    internal class AutoplayDisableSubmissionPatch : IInitializable
+    {
+        private readonly CommandLineParserResult _commandLineParserResult;
+        private readonly Submission _submission;
+
+        private AutoplayDisableSubmissionPatch(CommandLineParserResult commandLineParserResult, Submission submission)
+        {
+            _commandLineParserResult = commandLineParserResult;
+            _submission = submission;
+        }
+
+        public void Initialize()
+        {
+            if (_commandLineParserResult.Contains(BSAppInit.kAutoPlayOption))
+            {
+                _submission.DisableScoreSubmission(nameof(SongCore), "Autoplay is enabled.");
+            }
+        }
+    }
+}
